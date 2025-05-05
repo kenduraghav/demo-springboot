33ca6382ab287a6ac45528d7d284b52a1be3cf00
kenduraghav <kenduraghav@gmail.com>
2025-05-05 23:26:48 +0800

    Upgraded to Springboot 3.4.x and Springdoc-OpenApi v3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,28 +1,91 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @SpringBootApplication
-@EnableSwagger2
 public class DemoApplication {
-	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
+
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+class UserControllerV1{
+	
+	@Operation(summary = "Get User (v1)", description = "Returns user as plain text")
+	@GetMapping
+	String getusers() {
+		return "Users from V1";
+	}
+}
+
+
+
+@RestController
+@RequestMapping("/api/v2/users")
+class UserControllerV2 {
+	
+	
+	List<UserResponseDto> users = getUserResponseDtos();
+	
+	
+	@GetMapping
+	List<UserResponseDto> getUsers(){
+		return  users;
+		
+		
+	}
+	
+	
+	private List<UserResponseDto> getUserResponseDtos() {
+		return List.of(
+				new UserResponseDto("user1", "user1@gmail.com"),
+				new UserResponseDto("user2", "user2@gmail.com"),
+				new UserResponseDto("user3", "user3@gmail.com"),
+				new UserResponseDto("user4", "user4@gmail.com"),
+				new UserResponseDto("user5", "user5@gmail.com")
+				);
+	}
+
+
+	@PostMapping
+	UserResponseDto saveUsers(@Valid @RequestBody UserRequestDto dto) {
+		UserResponseDto userResponseDto = new UserResponseDto(dto.name(),dto.email());
+		return userResponseDto;
+	}
+	
+}
+
+record UserRequestDto(
+		@NotBlank 
+		String name,
+		
+		@NotBlank
+		@Email
+		String email) {
+	
+}
+
+
+
+record UserResponseDto(String name,String email) {}
--- src/main/java/com/example/demo/controllers/FruitResource.java
+++ src/main/java/com/example/demo/controllers/FruitResource.java
@@ -2,22 +2,12 @@ package com.example.demo.controllers;
 
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.model.Fruit;
-
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -32,32 +22,32 @@ public class FruitResource {
 		fruits.add(new Fruit("Green Apple", "Winter fruit"));
 	}
 
-	@ApiOperation(value = "View a list of fruits or returns the fruits list based on the description given in query parameter", produces = "application/json", response = List.class)
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
-			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
-	@RequestMapping(value = "/fruits",  method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+//	@ApiOperation(value = "View a list of fruits or returns the fruits list based on the description given in query parameter", produces = "application/json", response = List.class)
+//	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+//			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+//			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+//			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	@GetMapping(value = "/fruits", produces=MediaType.APPLICATION_JSON_VALUE)
 	public Set<Fruit> list(@RequestParam(required=false) String desc) {
 		return (desc == null) ? fruits : fruits.stream().filter(f-> f.getDescription().contains(desc)).collect(Collectors.toSet());
 	}
 	
-	@ApiOperation(value = "Returns a single fruit for the path parameter. Query param is case sensitive", response = Fruit.class)
-	@RequestMapping(value="/fruits/{name}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Fruit findByName(@PathVariable(value="name") String name) {
+	//@ApiOperation(value = "Returns a single fruit for the path parameter. Query param is case sensitive", response = Fruit.class)
+	@GetMapping(value="/fruits/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Fruit findByName(@PathVariable String name) {
 		return fruits.stream().filter(f -> f.getName().equals(name)).findAny().orElse(null);
 	}
 	
 
-	@ApiOperation(value = "Adds a fruit to the Set")
-	@RequestMapping(value = "/fruits", method = RequestMethod.POST)
+	//@ApiOperation(value = "Adds a fruit to the Set")
+	@PostMapping("/fruits")
 	public Set<Fruit> add(Fruit fruit) {
 		fruits.add(fruit);
 		return fruits;
 	}
 
-	@ApiOperation(value = "Deletes a fruit from the Set")
-	@RequestMapping(value = "/fruits", method = RequestMethod.DELETE)
+	//@ApiOperation(value = "Deletes a fruit from the Set")
+	@DeleteMapping("/fruits")
 	public Set<Fruit> delete(Fruit fruit) {
 		fruits.remove(fruit);
 		return fruits;
